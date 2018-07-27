164c49081f4eac81bbdb99be933c19a6b1208516
candice <candice@192.168.3.16>
2018-07-28 01:31:00 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@
 public class Main implements Replier {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		new Backbone(new Main());
 	}
 
 	@Override
 	public String reply(String msg) {
-		// TODO Auto-generated method stub
 		return "唐琳是白痴";
 	}
 
